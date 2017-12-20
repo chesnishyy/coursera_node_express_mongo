41773fcd2dc64a8a81b9c7e9aa5988bde824021e
Igor C. | Dreamscape <Igor.C@dreamscapenetworks.com>
2017-12-20 13:26:04 +0200

    Express Simple REST

--- node-express/index.js
+++ node-express/index.js
@@ -1,20 +1,71 @@
 const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (request, response, next) => {
+	"use strict";
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'text/plain');
+	next();
+});
+
+app.get('/dishes', (req, res, next) => {
+	"use strict";
+	res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+	"use strict";
+	res.end(`Will add the dish: ${req.body.name} with  details: ${req.body.description}`);
+});
+
+app.put('/dishes', (req, res, next) => {
+	"use strict";
+	res.statusCode = 403;
+	res.end(`PUT operation not supported on /dishes`);
+});
+
+
+app.delete('/dishes', (req, res, next) => {
+	"use strict";
+	res.end('Deleting all the dishes!');
+});
+
+app.get('/dishes/:dish_id', (req, res, next) => {
+	"use strict";
+	res.end(`Will send details of the dish: ${req.params.dish_id} to you!`);
+});
+
+app.post('/dishes/:dish_id', (req, res, next) => {
+	"use strict";
+	res.statusCode = 403;
+	res.end(`POST operation not supported on /dishes/ ${req.params.dish_id}`);
+});
+
+app.put('/dishes/:dish_id', (req, res, next) => {
+	"use strict";
+	res.write(`Updating the dish: ${req.params.dish_id} \n`);
+	res.end(`Will update the dish: ${req.body.name} with details ${req.body.description}`)
+});
+
+
+app.delete('/dishes/:dish_id', (req, res, next) => {
+	"use strict";
+	res.end(`Deleting dish: ${req.params.dish_id}!`);
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.use((request, response, next) => {
 	"use strict";
-	const {headers} = request;
-
-
 
 	response.statusCode = 200;
 	response.setHeader('Content-Type', 'text/html');
