eb7a8def4a25ed709a653984b999eec75bd3af11
Igor C. | Dreamscape <Igor.C@dreamscapenetworks.com>
2017-12-21 14:11:47 +0200

    Node MongoDB Example 1

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,30 +1,39 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = 'mongodb://127.0.0.1:27017/conFusion';
 
-MongoClient.connect(url, (err, db) => {
+MongoClient.connect(url, (err, client) => {
 	"use strict";
 	assert.equal(err, null);
 
 	console.log(`Connected correctly to server`);
 
+	const dbName = 'conFusion';
+	const db = client.db(dbName);
 	const collection = db.collection('dishes');
 
 	collection.insertOne({
 		'name': 'Utappizza',
 		'description': 'test'
-	})
-		.then((result) => {
-			console.log('After Insert: \n');
-			console.log(result.ops);
-			return result;
-		})
-		.find({}).toArray()
-		.then((docs) => {
-			console.log('Found: \n')
-			console.log(docs)
-		});
+	}, (err, result) => {
+		assert.equal(err, null);
 
-	db.dropCollection('dishes')
+		console.log('After Inseert: \n');
+		console.log(result.ops);
+
+		collection.find({}).toArray((err, docs) => {
+			assert.equal(err, null);
+
+			console.log('Found: \n');
+			console.log(docs);
+
+			db.dropCollection('dishes', (err, result) => {
+				assert.equal(err, null);
+
+				client.close();
+			})
+		})
+	});
+
 });
