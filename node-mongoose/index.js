738402d55558a4fe761ca7955ae6a21005a395dd
Igor Chesnishyy <i.chesnishyy@gmail.com>
2018-01-06 14:44:34 +0200

    Node Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -6,31 +6,28 @@ const Dishes = require('./models/dishes');
 const url = 'mongodb://127.0.0.1:27017/conFusion';
 const connect = mongoose.connect(url);
 
-connect.then((database) => {
+connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'some description'
+    Dishes.create({
+        name: "Uthappizza",
+        description: 'TEST'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return db.connection.db.collection('dishes').drop();
+    })
+    .then(() => {
+        return db.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return database.connection.db.collection('dishes').drop();
-        })
-        .then(() => {
-            return database.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
 });
